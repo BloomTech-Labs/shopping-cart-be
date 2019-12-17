0c7327f64631846ed0d50f61d52264f0b0a6afec
DimejiAre <olardi2@gmail.com>
2019-12-17 21:09:59 +0100

    chore: add tests for login seller endpoint

---
+++ tests/routes/loginSeller.test.js
@@ -0,0 +1,72 @@
+const request = require('supertest')
+const server = require('../../server')
+const Seller = require('../../models/seller')
+
+async function clearDb () {
+  await Seller.deleteMany({})
+}
+
+beforeAll(async () => {
+  try {
+    await clearDb()
+  } catch (error) {
+    console.error(error.name, error.message)
+  }
+})
+
+describe('loginSeller', () => {
+  it('expects 400 response if no user login data', async () => {
+    const res = await request(server).post('/api/auth/login')
+    expect(res.status).toBe(400)
+  })
+  it('expects Password field is required error message', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({
+        phone: '1112223333'
+      })
+    expect(res.status).toBe(400)
+    expect(res.body).toEqual({ password: 'Password field is required' })
+  })
+  it('expects Password length is not less than 6', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({
+        phone: '2223335555',
+        password: 'Pass'
+      })
+    expect(res.status).toBe(400)
+    expect(res.body).toEqual({
+      password: 'Password must be at least 6 characters'
+    })
+  })
+  it('expects Phone Number is invalid error message ', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({
+        phone: 'tolu',
+        password: 'Password12345'
+      })
+    expect(res.status).toBe(400)
+    expect(res.body).toEqual({ phone: 'Phone Number is invalid' })
+  })
+  it('checks if user has been logged in successfully with a token being returned', async () => {
+    await request(server)
+      .post('/api/auth/register')
+      .send({
+        phone: '07031900035',
+        password: 'Password12345',
+        password2: 'Password12345'
+      })
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({
+        phone: '07031900035',
+        password: 'Password12345'
+      })
+
+    const { token } = res.body
+    expect(res.status).toBe(200)
+    expect(token).toBeDefined()
+  })
+})
