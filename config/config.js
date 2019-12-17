58464e45d29883f27c4fd8ac1e7c5d6a18c06ecc
Rexben001 <rexben.rb@gmail.com>
2019-12-17 10:15:04 +0100

    chore: update config file for travis

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const { NODE_ENV } = process.env;
-let mongoURL = '';
+let mongoURL = "";
 
-if (NODE_ENV === 'test') {
-  mongoURL = process.env.DB_CONNECTION_TEST;
+if (NODE_ENV === "testing") {
+  // add: "mongodb://localhost/test" for travis config
+  mongoURL = process.env.DB_CONNECTION_TEST || "mongodb://localhost/test";
 } else {
   mongoURL = process.env.DB_CONNECTION;
 }
