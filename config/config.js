a1bf9e6d016bb03753aea073c2eef602b39e3f79
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-12-17 18:13:52 +0100

    bg: setup mongo URI for travis configuration

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const { NODE_ENV } = process.env
 let mongoURI = ''
 
 if (NODE_ENV === 'test') {
-  mongoURI = process.env.DB_CONNECTION_TEST
+  mongoURI = process.env.DB_CONNECTION_TEST || 'mongodb://localhost/test'
 } else {
   mongoURI = process.env.DB_CONNECTION
 }
