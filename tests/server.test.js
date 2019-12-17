97aec70ab39114bd9d2b6ac814c36c1bfdeab1e0
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-12-17 17:42:23 +0100

    ft: add tests and packages

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,17 +2,9 @@ const request = require('supertest')
 const server = require('../server')
 
 describe('index route', () => {
-<<<<<<< HEAD
   it('it runs', async () => {
     const res = await request(server).get('/')
     expect(res.text).toBe('Api is running!!')
     expect(res.status).toBe(200)
-=======
-  test('it runs', async done => {
-    const res = await request(server).get('/')
-    expect(res.text).toBe('Api is running!!')
-    expect(res.status).toBe(200)
-    done()
->>>>>>> 1627b2a29131be4c4cd93f62ae3176298d965081
   })
 })
