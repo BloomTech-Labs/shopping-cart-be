761be04a0f8e93643439516076ac03c3e95a8f71
DimejiAre <olardi2@gmail.com>
2019-12-18 15:52:31 +0100

    feat: create product model

---
+++ models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number
+    },
+    storeId: {
+        type: String,
+        required: true,
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
