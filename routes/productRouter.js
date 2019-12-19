9847c532b0f836f37dd31dcdfde9b82adb5fd376
DimejiAre <olardi2@gmail.com>
2019-12-19 16:08:15 +0100

    feat: add authentication to addProduct route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const {addProduct} = require('../controllers/product')
+const authenticate = require('../middleware/authenticateMiddleware')
 
-router.post('/:storeId/products/', addProduct)
+router.post('/:storeId/products/', authenticate, addProduct)
 
 module.exports = router;
