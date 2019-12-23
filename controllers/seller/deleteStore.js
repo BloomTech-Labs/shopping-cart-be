4bf97a1a16a44b933484e1ef27d368859c46fcc2
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-12-23 15:10:18 +0100

    ch: write tests for the refactored delete endpoint

--- controllers/seller/deleteStore.js
+++ controllers/seller/deleteStore.js
@@ -9,10 +9,10 @@ async function deleteStore(req, res) {
         .status(404)
         .json({ message: "There is no store associated with this account" });
     }
-    const store_id = store["_id"];
-    await Store.deleteOne({ _id:store_id });
+    const storeId = store["_id"];
+    await Store.deleteOne({ _id:storeId });
  
-    return res.status(200).json({ message: `${store.storeName} store has been removed`, store });
+    return res.status(200).json({ message: `${store.storeName} store has been removed`});
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- tests/routes/store/deleteStore.test.js
+++ tests/routes/store/deleteStore.test.js
@@ -1,97 +1,86 @@
-const request = require('supertest')
-const server = require('../../../server')
-const Store = require('../../../models/store')
+const request = require("supertest");
+const server = require("../../../server");
+const Store = require("../../../models/store");
 
-let token
-let token2
-let store_id
-let store_id2
+let token;
+let token2;
+let storeName;
 
-async function clearDb () {
-  await Store.deleteMany({})
+async function clearDb() {
+  await Store.deleteMany({});
 }
 beforeAll(async () => {
-  jest.setTimeout(10000)
+  jest.setTimeout(10000);
   try {
-    clearDb()
+    clearDb();
     const response1 = await request(server)
-      .post('/api/auth/register')
+      .post("/api/auth/register")
       .send({
-        phone: '0903190035',
-        password: 'password12345'
-      })
+        phone: "0903190035",
+        password: "password12345"
+      });
 
-    token = response1.body.token
+    token = response1.body.token;
 
     const response2 = await request(server)
-      .post('/api/auth/register')
+      .post("/api/auth/register")
       .send({
-        phone: '06023378722',
-        password: 'password12345'
-      })
+        phone: "06023378722",
+        password: "password12345"
+      });
 
-    token2 = response2.body.token
-
-    // create store for seller 1
-    const newStore = new Store({
-      storeName: 'Stone &  Sticks',
-      ownerName: 'Jane Doe',
-      currency: 'dollars',
-      imageUrl: 'some image',
-      seller: response1.body.user.id
-    })
-
-    store_id = newStore.id
-    newStore
-      .save()
-      .then(store => {})
-      .catch(err => console.log(err))
+    token2 = response2.body.token;
 
     // create store for seller 2
     const newStore2 = new Store({
-      storeName: 'icecreamfactory',
-      ownerName: 'Johnny Walker',
-      currency: 'Yen',
-      imageUrl: 'some image',
+      storeName: "icecreamfactory",
+      ownerName: "Johnny Walker",
+      currency: "Yen",
+      imageUrl: "some image",
       seller: response2.body.user.id
-    })
-
-    store_id2 = newStore2.id
+    });
+    storeName = newStore2.storeName;
     newStore2
       .save()
       .then(store => {})
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   } catch (error) {
-    console.error(error.name, error.message)
+    console.error(error.name, error.message);
   }
-})
+});
 
-describe('delete a store', () => {
-  it('returns No credentials provided', async () => {
-    const res = await request(server).delete('/api/store')
-    expect(res.body).toEqual({ message: 'No credentials provided' })
-    expect(res.status).toBe(400)
-  })
-  it('returns no store was found', async () => {
+describe("delete a store", () => {
+  it("returns No credentials provided", async () => {
+    const res = await request(server).delete("/api/store");
+    expect(res.body).toEqual({ message: "No credentials provided" });
+    expect(res.status).toBe(400);
+  });
+  it("returns no store was found", async () => {
     const res = await request(server)
-      .delete('/api/store')
-      .set('Authorization', token)
-    expect(res.status).toBe(404)
-    expect(res.body).toEqual({ message: 'No store was found' })
-  })
+      .delete("/api/store")
+      .set("Authorization", token);
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({
+      message: "There is no store associated with this account"
+    });
+  });
 
-  it('returns You can only delete your own store', async () => {
+  it("Store has been removed", async () => {
     const res = await request(server)
       .delete(`/api/store`)
-      .set('Authorization', token)
-    expect(res.status).toBe(400)
-    expect(res.body).toEqual({ message: 'You can only delete your own store' })
-  })
-  it('Store has been removed', async () => {
-    const res = await request(server)
-      .delete(`/api/store`)
-      .set('Authorization', token2)
-    expect(res.status).toBe(200)
-    expect(res.body).toEqual({ message: 'Store has been removed' })
-  })
-})
+      .set("Authorization", token2);
+    expect(res.status).toBe(200);
+
+    expect(res.body).toEqual({
+      message: `${storeName} store has been removed`
+    });
+  });
+});
+
+afterAll(async () => {
+  try {
+    await clearDb();
+  } catch (error) {
+    console.error(error.name, error.message);
+  }
+});
