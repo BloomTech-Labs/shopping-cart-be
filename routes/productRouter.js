83df26db31f2428aac721b0eccef441dc487f917
Rexben001 <rexben.rb@gmail.com>
2019-12-29 17:10:16 +0100

    ft: get products by store id

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,24 +1,24 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const {
   addProduct,
   getProducts,
   editProduct
-} = require('../controllers/product')
-const authenticate = require('../middleware/authenticateMiddleware')
+} = require("../controllers/product");
+const authenticate = require("../middleware/authenticateMiddleware");
 
 // @route POST api/store/:storeId/products
 // @desc  Add a new item to the store
 // @access Private
-router.post('/products/', authenticate, addProduct)
+router.post("/products/", authenticate, addProduct);
 
-// @route GET api/store/products
+// @route GET api/store/products/:store_id
 // @desc Gett all products from store
 // @access Public
-router.get('/products', authenticate, getProducts)
+router.get("/products/:store_id", authenticate, getProducts);
 
 // @route POST /api/store/products/:product_id
 // @desc Login user
 // @access Public
-router.put('/products/:product_id', authenticate, editProduct)
+router.put("/products/:product_id", authenticate, editProduct);
 
-module.exports = router
+module.exports = router;
--- tests/routes/getProducts.test.js
+++ tests/routes/getProducts.test.js
@@ -1,66 +1,70 @@
-const request = require('supertest')
-const server = require('../../server')
-const Product = require('../../models/product')
+const request = require("supertest");
+const server = require("../../server");
+const Product = require("../../models/product");
+const Store = require("../../models/store");
 
-let token
+let token;
+let storeId;
 
-async function clearDb () {
-  await Product.deleteMany({})
+async function clearDb() {
+  await Store.deleteMany({});
+  await Product.deleteMany({});
 }
 
 beforeAll(async () => {
   try {
-    await clearDb()
+    await clearDb();
     const response = await request(server)
-      .post('/api/auth/register')
+      .post("/api/auth/register")
       .send({
-        phone: '07031900078',
-        password: 'password12345'
-      })
-    token = response.body.token
+        phone: "07031900078",
+        password: "password12345"
+      });
+    token = response.body.token;
 
-    await request(server)
-      .post('/api/store')
+    const res = await request(server)
+      .post("/api/store")
       .send({
-        storeName: 'Glass &  Sticks',
-        ownerName: 'Jane Doe',
-        currency: 'dollars',
-        imageUrl: 'some image'
+        storeName: "Glass &  Sticks",
+        ownerName: "Jane Doe",
+        currency: "dollars",
+        imageUrl: "some image"
       })
-      .set('Authorization', token)
+      .set("Authorization", token);
+    storeId = res.body.saved["_id"];
   } catch (error) {
-    console.error(error.name, error.message)
+    console.error(error.name, error.message);
   }
-})
+});
 
-describe('get all products', () => {
-  test('should return no products found', async () => {
+describe("get all products", () => {
+  test("should return no products found", async () => {
     const response = await request(server)
-      .get('/api/store/products')
-      .set('Authorization', token)
-    expect(response.status).toBe(404)
-    expect(response.body).toEqual({ message: 'No products found' })
-  })
+      .get("/api/store/products/5e00a47af069c4278845cfb1")
+      .set("Authorization", token);
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ message: "No products found" });
+  });
 
-  test('should return one product', async () => {
+  test("should return one product", async () => {
     await request(server)
-      .post('/api/store/products')
+      .post("/api/store/products")
       .send({
-        name: 'product1',
-        description: 'some description here',
-        stock: '1',
-        price: '200'
+        name: "product1",
+        description: "some description here",
+        stock: "1",
+        price: "200"
       })
-      .set('Authorization', token)
+      .set("Authorization", token);
     const response = await request(server)
-      .get('/api/store/products')
-      .set('Authorization', token)
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveLength(1)
+      .get(`/api/store/products/${storeId}`)
+      .set("Authorization", token);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
     expect(response.body[0]).toHaveProperty(
-      'description',
-      'some description here'
-    )
-    expect(response.body[0]).toBeTruthy()
-  })
-})
+      "description",
+      "some description here"
+    );
+    expect(response.body[0]).toBeTruthy();
+  });
+});
