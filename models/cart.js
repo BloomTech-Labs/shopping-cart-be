2a0272469aa26cc8947c45f2e0ce9147cbd419f0
aapetsi <apetsiampiah@gmail.com>
2020-01-13 16:55:18 +0000

    fix: change buyer model to cart model

---
+++ models/cart.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+  store_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'store',
+    required: true
+  },
+  contents: {
+    type: Array,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  },
+  agreed_price: {
+    type: Number,
+    required: true
+  },
+  checked_out: {
+    type: Boolean,
+    required: true
+  },
+  checkout_date: {
+    type: Date,
+    required: true
+  },
+  paid_amount: {
+    type: Number,
+    required: true
+  }
+})
+
+const Cart = mongoose.model('cart', cartSchema)
+
+module.exports = Buyer
