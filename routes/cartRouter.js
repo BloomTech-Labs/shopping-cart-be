3f71529c30d6919c286054486e3f9753daab16c1
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-14 18:52:14 +0100

    ft: create api endpoint to edit products in a cart

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,11 +1,17 @@
 const router = require('express').Router()
 const {
-  addCart
+  addCart,
+  editCart
 } = require('../controllers/cart')
 
 // @route POST api/store/:store_id/cart
 // @desc  Add items to cart
 // @access Public
 router.post('/:store_id/cart', addCart)
+
+// @route PUT api/store/:store_id/cart
+// @desc  Add items to cart
+// @access Public
+router.put('/cart/:cart_id', editCart)
 
 module.exports = router
