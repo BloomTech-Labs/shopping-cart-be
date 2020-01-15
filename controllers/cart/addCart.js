ca44a2eccbec06e74d3b171d1d70b7744b79dbb2
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-15 17:07:36 +0100

    ch: pull latest from develop

--- controllers/cart/addCart.js
+++ controllers/cart/addCart.js
@@ -1,6 +1,10 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const Cart = require('../../models/cart')
 const Store = require('../../models/store')
 const validateCartInput = require('../../middleware/validateCartData')
+
 
 async function addCart (req, res) {
   const { errors, isValid } = validateCartInput(req.body)
@@ -21,7 +25,11 @@ async function addCart (req, res) {
     cart.storeId = store._id
     const newCart = new Cart(cart)
     const result = await newCart.save()
-    return res.status(200).json(result)
+
+    console.log(result)
+
+
+    //return res.status(200).json(result)
   } catch (err) {
     return res.status(500).json(err.message)
   }
