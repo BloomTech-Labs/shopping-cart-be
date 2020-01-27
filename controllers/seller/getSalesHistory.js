2b8ef82d8e9e9940e3da6144f3bfab8de5c8061b
aapetsi <apetsiampiah@gmail.com>
2020-01-27 13:26:35 +0000

    ft: add tests for sales history route

--- controllers/seller/getSalesHistory.js
+++ controllers/seller/getSalesHistory.js
@@ -5,10 +5,10 @@ const Product = require('../../models/product')
 async function getSalesHistory(req, res) {
   try {
     const storeId = req.params.store_id
-    if (!storeId) {
+    const store = await Store.findById({ _id: storeId })
+    if (!store) {
       return res.status(404).json({ message: 'No store found' })
     }
-
     const populatedCart = await Cart.aggregate([
       {
         $lookup: {
@@ -20,6 +20,10 @@ async function getSalesHistory(req, res) {
       }
     ])
 
+    if (populatedCart.length === 0) {
+      return res.status(404).json({ message: 'Cart is empty' })
+    }
+
     // find store related to carts
     const salesHistory = populatedCart.filter(
       item =>
@@ -29,7 +33,7 @@ async function getSalesHistory(req, res) {
     // calculate total sales made
     const totalSales = salesHistory.reduce((acc, item) => item.total + acc, 0)
 
-    res.status(200).json({ totalSales, salesHistory })
+    return res.status(200).json({ totalSales, salesHistory })
   } catch (error) {
     res.status(500).json(error.message)
   }
---
+++ tests/routes/store/getSalesHistory.test.js
@@ -0,0 +1,115 @@
+const request = require('supertest')
+const server = require('../../../server')
+const Product = require('../../../models/product')
+const Seller = require('../../../models/seller')
+const Store = require('../../../models/store')
+const Cart = require('../../../models/cart')
+
+let token
+let cartId
+let storeId
+let product1Id
+let product2Id
+
+async function clearDb() {
+  await Seller.deleteMany({})
+  await Store.deleteMany({})
+  await Cart.deleteMany({})
+}
+
+beforeAll(async () => {
+  jest.setTimeout(30000)
+  try {
+    await clearDb()
+    const response = await request(server)
+      .post('/api/auth/register')
+      .send({
+        phone: '+233276202069',
+        password: 'password12345'
+      })
+
+    token = response.body.token
+
+    const store = await request(server)
+      .post('/api/store')
+      .send({
+        storeName: 'Laptops & Phones',
+        ownerName: 'Jane Doe',
+        currency: 'dollars',
+        imageUrl: 'some image',
+        address: 'no 337 rous road'
+      })
+      .set('Authorization', token)
+    storeId = store.body.saved._id
+
+    //   create products
+    const product1 = await request(server)
+      .post('/api/store/products')
+      .send({
+        name: 'Shoes1',
+        description: 'A very nice',
+        price: 500,
+        stock: 10,
+        images: ['mee.jpg', 'us.jpg']
+      })
+      .set('Authorization', token)
+    product1Id = product1.body._id
+
+    const product2 = await request(server)
+      .post('/api/store/products')
+      .send({
+        name: 'Shoes2',
+        description: 'A very nice2',
+        price: 5000,
+        stock: 100,
+        images: ['mee2.jpg', 'us2.jpg']
+      })
+      .set('Authorization', token)
+
+    product2Id = product2.body._id
+
+    // add item to cart
+    const cart = await request(server)
+      .post(`/api/store/${storeId}/cart`)
+      .send({
+        agreedPrice: 40,
+        total: 400,
+        checkedOut: true,
+        email: 'test@gmail.com',
+        contents: [
+          { product: product1Id, quantity: 3 },
+          { product: product2Id, quantity: 3 }
+        ]
+      })
+    cartId = cart.body.result._id
+  } catch (error) {
+    console.error(error.name, error.message)
+  }
+})
+
+describe('get sales history route', () => {
+  it('should return store not found', async () => {
+    const response = await request(server)
+      .get(`/api/store/${product1Id}/sales`)
+      .set('Authorization', token)
+    expect(response.status).toBe(404)
+    expect(response.body.message).toBeDefined()
+  })
+
+  it('should return the sales history', async () => {
+    const response = await request(server)
+      .get(`/api/store/${storeId}/sales`)
+      .set('Authorization', token)
+    expect(response.status).toBe(200)
+    expect(response.body.totalSales).toBe(400)
+    expect(response.body.salesHistory).toBeDefined()
+  })
+
+  it('should return server related error', async () => {
+    const response = await request(server)
+      .get('/api/store/wrongid/sales')
+      .set('Authorization', token)
+    expect(response.status).toBe(500)
+    expect(response.body).toBeDefined()
+  })
+})
