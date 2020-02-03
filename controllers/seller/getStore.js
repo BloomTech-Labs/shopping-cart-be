b58ef6e1415484044e37419e19f1d5d105d77872
Rexben001 <rexben.rb@gmail.com>
2020-02-03 15:32:25 +0100

    update store to have seller phone

--- controllers/seller/getStore.js
+++ controllers/seller/getStore.js
@@ -1,26 +1,35 @@
-const Store = require('../../models/store')
-const mongoose = require('mongoose')
+const Store = require("../../models/store");
+const Seller = require("../../models/seller");
+const mongoose = require("mongoose");
 
 async function getOneStore(req, res) {
   try {
-    const sellerId = req.decodedToken ? req.decodedToken.sub : null
-    const store_id = req.params.store_id ? req.params.store_id : null
+    let seller = ''
+    const sellerId = req.decodedToken ? req.decodedToken.sub : null;
+    const store_id = req.params.store_id ? req.params.store_id : null;
 
     if (!mongoose.Types.ObjectId.isValid(store_id) && !sellerId) {
-      return res.status(404).json({ message: 'There is no store with that id' })
+      return res
+        .status(404)
+        .json({ message: "There is no store with that id" });
+    }
+
+    if (sellerId) {
+       seller = await Seller.findById(sellerId);
     }
 
     const store = await Store.findOne(
       sellerId ? { seller: sellerId } : { _id: store_id }
-    )
+    );
 
     if (!store) {
-      return res.status(404).json({ message: 'No store found' })
+      return res.status(404).json({ message: "No store found" });
     }
-    return res.status(200).json(store)
+    const updatedStore = { ...store._doc, phone: seller.phone };
+    return res.status(200).json(updatedStore);
   } catch (err) {
-    return res.status(500).json(err.message)
+    return res.status(500).json(err.message);
   }
 }
 
-module.exports = getOneStore
+module.exports = getOneStore;
