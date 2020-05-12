d145a84e9ea43ee1fb0d905a91a8200100f797a4
chirag <harithesia@gmail.com>
2020-05-11 20:33:55 -0500

    test for heroku pipeline

--- models/store.js
+++ models/store.js
@@ -1,23 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+//this is test commit for seller product creation branch. DELETE LATER
 const storeSchema = mongoose.Schema({
-  ownerName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  currency: { type: String, required: true },
-  imageUrl: { type: String },
-  storeName: { type: String, required: true, unique: true },
-  seller: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Seller'
-  },
-  address: { type: String, required: true },
-  register_date: {
-    type: Date,
-    default: Date.now
-  },
-  stripeId: { type: String }
-})
-const Store = mongoose.model('store', storeSchema)
-module.exports = Store
+	ownerName: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	currency: { type: String, required: true },
+	imageUrl: { type: String },
+	storeName: { type: String, required: true, unique: true },
+	seller: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Seller'
+	},
+	address: { type: String, required: true },
+	register_date: {
+		type: Date,
+		default: Date.now
+	},
+	stripeId: { type: String }
+});
+const Store = mongoose.model('store', storeSchema);
+module.exports = Store;
