171026b5b5b0a0612c659158a87ea4db9ca12d56
ChiragThesia <harithesia@gmail.com>
2020-05-14 20:44:57 -0500

    Update the product schema.

--- models/product.js
+++ models/product.js
@@ -1,34 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  stock: {
-    type: Number,
-    required: false
-  },
-  storeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'store',
-    required: true
-  },
-  images: [
-    {
-      type: String
-    }
-  ]
-})
+	productName: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: String,
+		required: true
+	},
+	category: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	images: [
+		{
+			type: String
+		}
+	],
+	vairants: [
+		{
+			type: String
+		}
+	],
 
-const Product = mongoose.model('product', productSchema)
+	storeId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'store',
+		required: true
+	}
+});
 
-module.exports = Product
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
