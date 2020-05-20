f1b2be94c0cddb8ae02541d65516e04f3885ef05
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-05-19 20:19:46 -0400

    fixed errors

--- server.js
+++ server.js
@@ -1,41 +1,22 @@
-<<<<<<< HEAD
-require('dotenv').config()
-const express = require('express')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const cors = require('cors')
-const path = require('path')
-const mongoose = require('mongoose')
-const mongoURI = require('./config/config')
-const passport = require('passport')
-=======
-require("dotenv").config();
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const path = require("path");
-const mongoose = require("mongoose");
-const mongoURI = require("./config/config");
-const passport = require("passport");
->>>>>>> de231171721cdd277e2889b6a4ed63986fc8ac1f
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const mongoURI = require('./config/config');
+const passport = require('passport');
 
-const stripeAuth = require("./authentication/stripeAuthentication");
-const authRouter = require("./routes/authRouter");
-const productRouter = require("./routes/productRouter");
-const storeRouter = require("./routes/storeRouter");
-const cartRouter = require("./routes/cartRouter");
-const paymentRouter = require("./routes/paymentRouter");
-const stripeAuthRouter = require("./routes/stripeAuthRouter");
-const orderRouter = require("./routes/orderRouter")
+const stripeAuth = require('./authentication/stripeAuthentication');
+const authRouter = require('./routes/authRouter');
+const productRouter = require('./routes/productRouter');
+const storeRouter = require('./routes/storeRouter');
+const cartRouter = require('./routes/cartRouter');
+const paymentRouter = require('./routes/paymentRouter');
+const stripeAuthRouter = require('./routes/stripeAuthRouter');
+const orderRouter = require('./routes/orderRouter');
 
-<<<<<<< HEAD
-server.use(morgan('dev'))
-server.use(helmet())
-server.use(express.json())
-server.use(express.urlencoded({ extended: false }))
-=======
 const server = express();
->>>>>>> de231171721cdd277e2889b6a4ed63986fc8ac1f
 
 server.use(helmet());
 server.use(express.json());
@@ -46,13 +27,13 @@ server.use(cors());
 server.use(passport.initialize());
 server.use(passport.session());
 
-server.use("/api/auth", authRouter);
-server.use("/api/store", productRouter);
-server.use("/api/store", storeRouter);
-server.use("/api/store", cartRouter);
-server.use("/api/store", orderRouter)
-server.use("/api/payment", paymentRouter);
-server.use("/api/auth/stripe", stripeAuthRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/store', productRouter);
+server.use('/api/store', storeRouter);
+server.use('/api/store', cartRouter);
+server.use('/api/store', orderRouter);
+server.use('/api/payment', paymentRouter);
+server.use('/api/auth/stripe', stripeAuthRouter);
 
 //new connection file.
 
@@ -65,31 +46,29 @@ passport.deserializeUser((user, done) => {
 
 passport.use(stripeAuth);
 
-server.use(express.static(path.join(__dirname, "public")));
-server.set("views", path.join(__dirname, "views"));
-server.set("view engine", "pug");
+server.use(express.static(path.join(__dirname, 'public')));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'pug');
 
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-<<<<<<< HEAD
-    useCreateIndex: true
-  })
-  .catch((err) => console.log(err))
-=======
-    useCreateIndex: true,
-  }, console.log("MongoDB connected"))
+  .connect(
+    mongoURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    },
+    console.log('MongoDB connected')
+  )
   .catch((err) => console.log(err));
->>>>>>> de231171721cdd277e2889b6a4ed63986fc8ac1f
 
-server.get("/", (req, res) => {
-  res.status(200).send("Api is running!!");
+server.get('/', (req, res) => {
+  res.status(200).send('Api is running!!');
 });
 
-server.all("*", (req, res) => {
-  res.status(404).json({ message: "This URL can not be found" });
+server.all('*', (req, res) => {
+  res.status(404).json({ message: 'This URL can not be found' });
 });
 
 module.exports = server;
