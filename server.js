663d83b590c409bcc387fc901c79d04444f2bc26
Ariunbold <ariun1118@gmail.com>
2020-06-02 19:38:28 -0400

    removed uncommented code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
-// const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
