6b2edc14b7321a54c567334c33cd34f1b8fdcc5f
chiragthesia <harithesia@gmail.com>
2020-07-16 20:35:57 -0500

    set up the payment intent

--- controllers/seller/getSellerFromStore.js
+++ controllers/seller/getSellerFromStore.js
@@ -4,18 +4,11 @@ const Store = require('../../models/store');
 const getSellerFromStore = async (req, res) => {
 	try {
 		const { storeId } = req.params;
-		console.log('StoreID?', storeId);
-
 		const store = await Store.findOne({ _id: storeId });
-		console.log('Is there a store', store);
-
 		const sellerId = store.seller;
 		const seller = await Seller.findOne({ _id: sellerId });
-		console.log('Seller?', seller);
-
-		const sellerPK = seller.stripe_publishable_key;
-
-		res.status(200).json(sellerPK);
+		const sellerStripeID = seller.stripe_user_id;
+		res.status(200).json(sellerStripeID);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -2,38 +2,45 @@
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 const router = require('express').Router();
 const stripeConfig = require('../config.stripe');
-const uuid = require('uuid');
+// const request = require('request');
 
 const stripe = require('stripe')(stripeConfig.stripe.secretKey);
 router.post('/create-payment-intent', async (req, res) => {
-	try {
-		const { price, pK } = req.body;
+	const { price, clientID } = req.body;
 
-		const idempontencyKey = uuid();
+	const paymentIntent = await stripe.paymentIntents.create(
+		{
+			payment_method_types: [ 'card' ],
+			amount: price * 100,
+			currency: 'usd'
+		},
+		{ stripeAccount: clientID }
+	);
 
-		const paymentIntentActual = await stripe.paymentIntents.create(
-			{
-				amount: price,
-				currency: 'usd'
-			}
-			// {
-			// 	idempontencyKey: idempontencyKey
-			// }
-		);
-
-		console.log('paymentIntent', paymentIntentActual);
-
-		//sending publishable key and payment intent to the client.
-		res.status(200).send({
-			message: `Thank you for your payment of $${price}`,
-			amount: price,
-			publishableKey: pK,
-			clientSecret: paymentIntentActual.clientSecret,
-			metadata: { integration_check: 'accept_a_payment' }
-		});
-	} catch (error) {
-		res.status(400).json({ message: 'Payment not processed' });
-	}
+	res.status(200).send({
+		amount: price,
+		publishableKey: stripeConfig.stripe.publishableKey,
+		clientSecret: paymentIntent.client_secret,
+		metadata: { integration_check: 'accept_a_payment' }
+	});
 });
+
+// router.get('/secret', async (req, res) => {
+// 	try {
+// 		const intent = await request
+// 			.post('http://localhost:4000/api/auth/stripe/create-payment-intent', {
+// 				price: 200,
+// 				pK:
+// 					'pk_test_51H2QaEI7mI0b1hERa7Hl22aT19XHAfYGpa4IyaP6LUR2vN7Khvu8nCSJIRPXOSKXVN32XVlH7eE0CfvO21vANYrB008zayIxxO',
+// 				stripeAccount: '123456'
+// 			})
+// 			.on('response', function(res) {
+// 				console.log(res.paymentIntentActual1559155);
+// 			});
+// 		res.status(200).json({ client_secret: intent.clientSecret });
+// 	} catch (error) {
+// 		res.status(400).json({ message: 'No secret for you' });
+// 	}
+// });
 
 module.exports = router;
