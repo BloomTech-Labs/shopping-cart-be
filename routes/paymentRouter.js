59f4b3165d1f5b6e79a91c42a9cacb4741294700
chiragthesia <harithesia@gmail.com>
2020-07-18 14:09:56 -0500

    changed stripe secret test key

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -8,25 +8,33 @@ const stripe = require('stripe')(stripeConfig.stripe.secretKey);
 router.post('/create-payment-intent', async (req, res) => {
 	const { price, clientID } = req.body;
 
-	try {
-		const paymentIntent = await stripe.paymentIntents.create(
+	if (!price || !clientID) {
+		return res
+			.status(422)
+			.json({ message: 'Please provide the required information. Price or Client id is missing. ' });
+	}
+
+	await stripe.paymentIntents
+		.create(
 			{
 				payment_method_types: [ 'card' ],
 				amount: price * 100,
 				currency: 'usd'
 			},
 			{ stripeAccount: clientID }
-		);
-
-		res.status(200).send({
-			amount: price,
-			publishableKey: stripeConfig.stripe.publishableKey,
-			clientSecret: paymentIntent.client_secret,
-			metadata: { integration_check: 'accept_a_payment' }
+		)
+		.then(function(paymentIntent) {
+			try {
+				res.status(200).send({
+					amount: price,
+					publishableKey: stripeConfig.stripe.publishableKey,
+					clientSecret: paymentIntent.client_secret,
+					metadata: { integration_check: 'accept_a_payment' }
+				});
+			} catch (error) {
+				res.status(500).send({ error: error.message });
+			}
 		});
-	} catch (error) {
-		res.status(400).send({ message: 'payment intent error' }, error.message);
-	}
 });
 
 module.exports = router;
